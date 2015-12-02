ede7cb526fb08d534d530e5d328b22822126fdeb
wbcustc <victorwang.bc@gmail.com>
2015-12-01 21:23:40 -0500

    read_npy

--- read_data.py
+++ read_data.py
@@ -1,31 +1,35 @@
 import numpy as np
 
-path_train_3k = './CS5785-final-data/train.txt'
-path_test_3k = './CS5785-final-data/test.txt'
-path_attr_list = './CS5785-final-data/attributes_list.txt'
-path_attr_train = './CS5785-final-data/attributes_train.txt'
-path_attr_test = './CS5785-final-data/attributes_train.txt'
+train_3k = './CS5785-final-data/train.txt'
+test_3k = './CS5785-final-data/test.txt'
+attr_list = './CS5785-final-data/attributes_list.txt'
+attr_train = './CS5785-final-data/attributes_train.txt'
+attr_test = './CS5785-final-data/attributes_train.txt'
+alexnet_train = './CS5785-final-data/alexnet_feat_train.npy'
+alexnet_test = './CS5785-final-data/alexnet_feat_test.npy'
+siftbow_train = './CS5785-final-data/SIFTBoW_train.npy'
+siftbow_test = './CS5785-final-data/SIFTBoW_test.npy'
 
-def read_train_3k(path = '../CS5785-final-data/train.txt'):
-        reader = open(path)
-        piclabel_3k = []
-        picname_3k = []
-        labels_3k = []
-        labels_num = {}
-        for line in reader:
-                line = line.strip()
-                line = line.split(' ')
-                jpg_name = line[0]
-                label = line[1]
-                picname_3k.append(jpg_name)
-                if labels_num.has_key(label):
-                        piclabel_3k.append(labels_num[label])
-                else:
-                        labels_num[label] = len(labels_3k)
-                        piclabel_3k.append(labels_num[label])
-                        labels_3k.append(label)
+def read_train_3k(path):
+    reader = open(path)
+    piclabel_3k = []
+    picname_3k = []
+    labels_3k = []
+    labels_num = {}
+    for line in reader:
+        line = line.strip()
+        line = line.split(' ')
+        jpg_name = line[0]
+        label = line[1]
+        picname_3k.append(jpg_name)
+        if labels_num.has_key(label):
+            piclabel_3k.append(labels_num[label])
+        else:
+            labels_num[label] = len(labels_3k)
+            piclabel_3k.append(labels_num[label])
+            labels_3k.append(label)
 	piclabel_3k = np.array(piclabel_3k)
-        return piclabel_3k, picname_3k, labels_3k
+    return piclabel_3k, picname_3k, labels_3k
 
 def read_test_3k(path):
     reader=open(path)
@@ -52,11 +56,8 @@ def read_attributes(path):
 			ret.append(vector)
 	return np.array(ret)
 
-def read_data():
-	return
+def read_npy(path):
+	ret = np.load(path)
+	return ret
 
-
-if __name__ == '__main__':
-	ret = read_attributes_train('./CS5785-final-data/attributes_train.txt')
-	print type(ret)
 
