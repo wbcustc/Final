3a0c4263466b98336c63093be693b29a58e2eaaa
liudan30 <lx98@cornell.edu>
2015-12-01 20:58:59 -0500

    add 3k

--- read_data.py
+++ read_data.py
@@ -2,24 +2,24 @@ import numpy as np
 
 def read_train_3k(path = '../CS5785-final-data/train.txt'):
         reader = open(path,'rb')
-        piclabel = list()
-        picname = list()
-        labels = list()
+        piclabel_3k = list()
+        picname_3k = list()
+        labels_3k = list()
         labels_num = dict()
         for line in reader:
                 line = line.strip()
                 line = line.split(' ')
                 jpg_name = line[0]
                 label = line[1]
-                picname.append(jpg_name)
+                picname_3k.append(jpg_name)
                 if labels_num.has_key(label):
-                        piclabel.append(labels_num[label])
+                        piclabel_3k.append(labels_num[label])
                 else:
                         labels_num[label] = len(labels)
-                        piclabel.append(labels_num[label])
-                        labels.append(label)
-	piclabel = np.array(piclabel)
-        return piclabel, picname, labels
+                        piclabel_3k.append(labels_num[label])
+                        labels_3k.append(label)
+	piclabel_3k = np.array(piclabel_3k)
+        return piclabel_3k, picname_3k, labels_3k
 
 def read_test_3k(path):
     reader=open('/Users/jingjingzhang/Desktop/Data_Minning_Final/CS5785-final-data/test.txt','rb')
