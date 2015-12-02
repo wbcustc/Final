23a8616bcb08c0e8388abbb52d47b3dc1049fc5c
wbcustc <victorwang.bc@gmail.com>
2015-12-01 20:59:31 -0500

    read2

--- read_data.py
+++ read_data.py
@@ -1,3 +1,10 @@
+import numpy as np
+
+path_train_3k = './CS5785-final-data/train.txt'
+path_test_3k = './CS5785-final-data/test.txt'
+path_attr_list = './CS5785-final-data/attributes_list.txt'
+path_attr_train = './CS5785-final-data/attributes_train.txt'
+path_attr_test = './CS5785-final-data/attributes_train.txt'
 
 def read_train_3k(path):
 	return
@@ -5,8 +12,26 @@ def read_train_3k(path):
 def read_test_3k(path):
 	return
 
-def read_attributes_train(path):
+def read_attributes_list(path):
+	ret = []
+	with open(path) as filein:
+		for line in filein:
+			line = line.strip()
+			ret.append(line)
+	return ret
+
+def read_attributes(path):
+	ret = []
+	with open(path) as filein:
+		for line in filein:
+			feature = line.split(' ')[1].strip()
+			vector = [int(e) for e in feature.split(',')]
+			ret.append(vector)
+	return np.array(ret)
+
+def read_data():
 	return
 
-def read_attributes_test(path):
-	return
+if __name__ == '__main__':
+	ret = read_attributes_train('./CS5785-final-data/attributes_train.txt')
+	print type(ret)
